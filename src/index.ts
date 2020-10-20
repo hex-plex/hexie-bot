e8c066e6c8dce9329a81b646bd5d67daa738ac2b
HexPlex <sendhilsom7@gmail.com>
2020-10-20 16:37:35 +0530

    Have to fill in github api calls

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,40 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
-
-export = (app: Application) => {
+module.exports = (app) => {
   app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
-  })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' });
+    app.log.info(context);
+    await context.github.issues.createComment(issueComment);
+  });
+  app.on('issue_comment.created',async (context) => {
+    const issueComment = context.issue({ body: 'Thanks for the insight'});
+    app.log.info(context);
+    await context.github.issues.createComment(issueComment);
+  });
+  const router = app.route('/my-app');
+  router.use(require('express').static('public'));
+  router.get('/hello-world',(req,res) => {
+    res.send('HelloWorld');
+  });
+  router.post('/Issue', (req,res) => {
+    const password = req.headers.authentication;
+    if(password === process.env.PASSWORD){
+      // Send a request to github through octokit
+      app.log.info({body:"Worked"});
+    }
+    res.status(200).send('Invalid Password!');
+  });
+  router.delete('/Issue', (req,res) => {
+    const password = req.headers.authentication;
+    if(password === process.env.PASSWORD){
+      // Send a close request to github through octokit
+      app.log.info({body:"Worked"});
+    }
+    res.status(200).send('Invalid Password!');
+  });
+  router.patch('/Issue', (req, res) => {
+    const password = req.headers.authentication;
+    if(password === process.env.PASSWORD){
+      // Should edit the main issue comment
+      app.log.info({body:"Worked"});
+    }
+  });
+};
