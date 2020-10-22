910bc7520216c64a6e0809531a171b843cd31951
Vikhyath08 <vikhyath.pv@gmail.com>
2020-10-22 16:17:51 +0530

    Added method to create and update issues

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,91 @@
 module.exports = (app) => {
   app.on('issues.opened', async (context) => {
+    if(context.payload.sender.type != "Bot"){
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' });
     app.log.info(context);
     await context.github.issues.createComment(issueComment);
+    }
   });
   app.on('issue_comment.created',async (context) => {
-    if(context.sender.type != "Bot"){
+    app.log.info(context);
+    if(context.payload.sender.type != "Bot"){
       const issueComment = context.issue({ body: 'Thanks for the insight'});
-      app.log.info(context);
       await context.github.issues.createComment(issueComment);
     }
   });
   const router = app.route('/my-app');
   router.use(require('express').static('public'));
+  router.use(require('express').json())
   router.get('/hello-world',(req,res) => {
     res.send('HelloWorld');
   });
+
+  const createIssue = async function (issue, app) {
+    const github = await app.auth(12536268); // Insert Installation ID here
+    const owner = issue.owner;
+    const repo = issue.repo;
+    const title = issue.title;
+    console.log(title);
+    // const body = issue.body;
+    return await github.issues.create({
+      owner: owner,
+      repo: repo,
+      title: title,
+    });
+  }
+  
+  const updateIssue = async function (issue, app, issueId) {
+    const github = await app.auth(12536268); // Insert Installation ID here
+    const owner = issue.owner;
+    const repo = issue.repo;
+    const title = issue.title;
+    console.log(title);
+    // const body = issue.body;
+    return await github.issues.update({
+      owner: owner,
+      repo: repo,
+      issue_number: issueId,
+      title: title,
+    });
+  }
+
   router.post('/Issue', (req,res) => {
     const password = req.headers.authentication;
     if(password === process.env.PASSWORD){
       // Send a request to github through octokit
+      console.log(req.body.title)
+      const issue = {
+        owner: 'Vikhyath08',
+        repo: 'solid-octo-spoon',
+        title: req.body.title,
+        // labels: req.query.labels,
+        // body: req.query.body,
+        // assignees: req.query.as
+      }
+      createIssue(issue, app).then(
+        res.send('Success')
+      ).catch(err => console.log(err))
       app.log.info({body:"Worked"});
     }
     res.status(200).send('Invalid Password!');
   });
-  router.delete('/Issue', (req,res) => {
-    const password = req.headers.authentication;
-    if(password === process.env.PASSWORD){
-      // Send a close request to github through octokit
-      app.log.info({body:"Worked"});
-    }
-    res.status(200).send('Invalid Password!');
-  });
-  router.patch('/Issue', (req, res) => {
+
+  router.patch('/Issue/:id/', (req, res) => {
     const password = req.headers.authentication;
     if(password === process.env.PASSWORD){
       // Should edit the main issue comment
+      const issueId = req.params.id;
+      const issue = {
+        owner: 'Vikhyath08',
+        repo: 'solid-octo-spoon',
+        title: req.body.title,
+        // labels: req.query.labels,
+        // body: req.query.body,
+        // assignees: req.query.as
+      }
+      updateIssue(issue, app, issueId).then(
+        res.send('Success')
+      ).catch(err => console.log(err))
       app.log.info({body:"Worked"});
     }
   });
